dfcb0767a02c7c051816eb732b36b90976cb5a54
Derek <dereknugroho@gmail.com>
2025-11-24 10:47:29 -0400

    Move Kraken API key retrieval to auth module, capitalize global constants

--- src/cobit/utils/config.py
+++ src/cobit/utils/config.py
@@ -7,11 +7,5 @@ def load_config():
     config_path = files("cobit").joinpath("config.json")
     return json.loads(config_path.read_text())
 
-config = load_config()
-
-def get_kraken_keys():
-    """Safely attempt to retrieve Kraken API keys."""
-    try:
-        return os.environ["API_KEY_KRAKEN"], os.environ["API_SEC_KRAKEN"]
-    except KeyError:
-        raise RuntimeError("Kraken API keys not set in environment.")
+CONFIG = load_config()
+ENDPOINTS = CONFIG['endpoints']
