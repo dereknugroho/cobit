80eaa9eb06abafc658284b3e24eee07ebf0d35c6
Derek <dereknugroho@gmail.com>
2025-11-24 10:38:28 -0400

    Capitalize imported constants, improve function names

--- src/cobit/kraken/kraken_services.py
+++ src/cobit/kraken/kraken_services.py
@@ -1,31 +1,31 @@
 from cobit.kraken.kraken_client import make_request
-from cobit.utils.config import config
+from cobit.utils.config import CONFIG, ENDPOINTS
 
 def system_status():
     """Retrieve the current system status."""
-    return make_request(config['endpoints']['system_status'])
+    return make_request(ENDPOINTS['system_status'])
 
 def server_time():
     """Retrieve the current server time."""
-    return make_request(config['endpoints']['server_time'])
+    return make_request(ENDPOINTS['server_time'])
 
-def order_book(pair: str = config['currency_pairs']['BTC_USD']):
+def order_book(pair: str = CONFIG['currency_pairs']['BTC_USD']):
     """Retrieve the live order book for a given currency pair."""
     query = {'pair': pair}
 
-    return make_request(path=config['endpoints']['order_book'], query=query)
+    return make_request(path=ENDPOINTS['order_book'], query=query)
 
-def recent_trades(pair: str = config['currency_pairs']['BTC_USD'], count: int = 1000):
+def recent_market_trades(pair: str = CONFIG['currency_pairs']['BTC_USD'], count: int = 1000):
     """Retrieve the most recent trades for a given currency pair."""
     query = {
         'pair': pair,
         'count': count,
     }
 
-    return make_request(path=config['endpoints']['recent_trades'], query=query)
+    return make_request(path=ENDPOINTS['recent_market_trades'], query=query)
 
-def recent_spreads(pair: str = config['currency_pairs']['BTC_USD']):
+def recent_market_spreads(pair: str = CONFIG['currency_pairs']['BTC_USD']):
     """Retrieve the live order book for a given currency pair."""
     query = {'pair': pair}
 
-    return make_request(path=config['endpoints']['recent_spreads'], query=query)
+    return make_request(path=ENDPOINTS['recent_market_spreads'], query=query)
