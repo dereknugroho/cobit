91724a058002ad06a01e0e4225cd324f89a2f701
Derek <dereknugroho@gmail.com>
2025-11-24 10:46:57 -0400

    Test Kraken API call functionality

--- src/cobit/main.py
+++ src/cobit/main.py
@@ -1,11 +1,17 @@
-from cobit.kraken.kraken_services import server_time, order_book, recent_trades, recent_spreads
+from cobit.kraken.kraken_services import system_status, server_time, order_book, recent_market_trades, recent_market_spreads
 
 if __name__ == '__main__':
-    # server_time = server_time()
-    # print(f'---------------')
-    # print(f'| Server Time |')
-    # print(f'---------------')
-    # print(f'{server_time}')
+    system_status = system_status()
+    print(f'-----------------')
+    print(f'| System Status |')
+    print(f'-----------------')
+    print(f'{system_status}')
+
+    server_time = server_time()
+    print(f'---------------')
+    print(f'| Server Time |')
+    print(f'---------------')
+    print(f'{server_time}')
 
     # order_book = order_book()
     # print(f'--------------')
@@ -13,14 +19,14 @@ if __name__ == '__main__':
     # print(f'--------------')
     # print(f'{order_book}')
 
-    # recent_spreads = recent_spreads()
+    # recent_spreads = recent_market_spreads()
     # print(f'------------------')
     # print(f'| Recent Spreads |')
     # print(f'------------------')
     # print(f'{recent_spreads}')
 
-    recent_trades = recent_trades()
-    print(f'-----------------')
-    print(f'| Recent Trades |')
-    print(f'-----------------')
-    print(f'{recent_trades}')
+    # recent_trades = recent_market_trades()
+    # print(f'-----------------')
+    # print(f'| Recent Trades |')
+    # print(f'-----------------')
+    # print(f'{recent_trades}')
