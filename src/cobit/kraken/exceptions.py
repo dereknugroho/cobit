e3b0efb77b465d02663b5830d39c15648cc4d98e
Derek <dereknugroho@gmail.com>
2025-11-26 13:07:36 -0400

    Create custom exception class for Kraken API errors

---
+++ src/cobit/kraken/exceptions.py
@@ -0,0 +1,7 @@
+class KrakenAPIError(Exception):
+    def __init__(self, errors, *args):
+        super().__init__(*args)
+        self.errors = errors
+
+    def __str__(self):
+        return f"Kraken API error(s): {self.errors}"
