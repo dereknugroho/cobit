aaca38428b752d80d3b28d736fbffe0e210ace7b
Derek <dereknugroho@gmail.com>
2025-11-26 13:03:42 -0400

    Implement and test order book ingestion, use custom exception class for Kraken API errors, move default argument values to highest-level functions

--- src/cobit/ingestion/ingestion_pipeline.py
+++ src/cobit/ingestion/ingestion_pipeline.py
@@ -1,24 +1,46 @@
 from cobit.ingestion.market_trades_ingestion import generate_market_trades_table
-from cobit.kraken.kraken_services import recent_market_trades
+from cobit.ingestion.order_book_ingestion import generate_order_book_table
+from cobit.kraken.exceptions import KrakenAPIError
+from cobit.kraken.kraken_services import market_trades, order_book
 from cobit.utils.config import CONFIG
 
-def ingest_market_trades(pair: str):
-    """Fetch Kraken trades for a pair and convert them into a PyArrow table."""
-    response = recent_market_trades(pair=pair)
+def ingest_market_trades(pair: str = 'BTC/USD', count: int = 1000):
+    """Fetch Kraken trades for a currency pair and convert them into a PyArrow table."""
+    response = market_trades(pair, count)
 
     # Handle Kraken error format
     if response.get('error'):
-        raise ValueError(f'Kraken API error: {response["error"]}')
+        raise KrakenAPIError(response['error'])
 
     raw_trades = response['result'][pair]
     table = generate_market_trades_table(raw_trades)
 
     return table
 
+def ingest_order_book(pair: str = 'BTC/USD', count: int = 100):
+    """Fetch Kraken order book for a currency pair and convert into a PyArrow table."""
+    response = order_book(pair, count)
+
+    # Handle Kraken error format
+    if response.get('error'):
+        raise KrakenAPIError(response['error'])
+
+    raw_order_book = response['result'][pair]
+    table = generate_order_book_table(raw_order_book)
+
+    return table
+
 if __name__ == '__main__':
-    ingested_market_trades = ingest_market_trades(CONFIG['currency_pairs']['BTC_USD'])
+    ingested_market_trades = ingest_market_trades(CONFIG['currency_pairs']['BTC_USD'], 100)
     print(f'--------------------------')
     print(f'| ingested_market_trades |')
     print(f'--------------------------')
     print(f'{type(ingested_market_trades)}')
     print(f'{ingested_market_trades}')
+
+    ingested_order_book = ingest_order_book(CONFIG['currency_pairs']['BTC_USD'], 10)
+    print(f'-----------------------')
+    print(f'| ingested_order_book |')
+    print(f'-----------------------')
+    print(f'{type(ingested_order_book)}')
+    print(f'{ingested_order_book}')
