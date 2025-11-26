d6a748406b5f144b20544598e459c91c7251c852
Derek <dereknugroho@gmail.com>
2025-11-26 13:06:27 -0400

    Move default arguments to higher level functions, remove useless functions

--- src/cobit/kraken/kraken_services.py
+++ src/cobit/kraken/kraken_services.py
@@ -9,23 +9,20 @@ def server_time():
     """Retrieve the current server time."""
     return make_request(ENDPOINTS['server_time'])
 
-def order_book(pair: str = 'BTC/USD'):
+def order_book(pair: str, count: int):
     """Retrieve the live order book for a given currency pair."""
-    query = {'pair': pair}
+    query = {
+        'pair': pair,
+        'count': count,
+    }
 
     return make_request(path=ENDPOINTS['order_book'], query=query)
 
-def recent_market_trades(pair: str = 'BTC/USD', count: int = 1000):
+def market_trades(pair: str, count: int):
     """Retrieve the most recent trades for a given currency pair."""
     query = {
         'pair': pair,
         'count': count,
     }
 
-    return make_request(path=ENDPOINTS['recent_market_trades'], query=query)
-
-def recent_market_spreads(pair: str = CONFIG['currency_pairs']['BTC_USD']):
-    """Retrieve the live order book for a given currency pair."""
-    query = {'pair': pair}
-
-    return make_request(path=ENDPOINTS['recent_market_spreads'], query=query)
+    return make_request(path=ENDPOINTS['market_trades'], query=query)
