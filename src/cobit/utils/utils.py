d6b5a2b78cdf44260fea66de4fa97e59fe275d10
Derek <dereknugroho@gmail.com>
2025-11-27 20:37:39 -0400

    Implement helper for generating a human-readable timestamp for prepending to filenames

---
+++ src/cobit/utils/utils.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+def generate_filename_timestamp() -> str:
+    """Generate a timestamp 'yyyymmdd-hhmmss_' for prepending to filenames."""
+    return datetime.now().strftime('%Y%m%d-%H%M%S_')
