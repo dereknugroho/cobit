8f7c9a54cf7f2a57564ce3bf60c56a0101176735
Derek <dereknugroho@gmail.com>
2025-11-28 13:54:58 -0400

    Refactor from config changes, implement logging

--- src/cobit/ingestion/ingestion_pipeline.py
+++ src/cobit/ingestion/ingestion_pipeline.py
@@ -2,8 +2,15 @@ from cobit.ingestion.market_trades_ingestion import generate_market_trades_table
 from cobit.ingestion.order_book_ingestion import generate_order_book_table
 from cobit.kraken.exceptions import KrakenAPIError
 from cobit.kraken.kraken_services import market_trades, order_book
-from cobit.utils.config import CONFIG, FILEPATHS
+from cobit.utils.config import CONFIG, PATHS
 from cobit.utils.io import save_raw_api_response_json
+from cobit.utils.logger import logger_setup, INFO
+
+logger = logger_setup(
+    __name__,
+    CONFIG['logging']['ingestion_log'],
+    INFO
+)
 
 def ingest_market_trades(pair: str = 'BTC/USD', count: int = 1000):
     """Fetch Kraken trades for a currency pair and convert them into a PyArrow table."""
@@ -13,17 +20,19 @@ def ingest_market_trades(pair: str = 'BTC/USD', count: int = 1000):
     # Handle Kraken error format
     if response.get('error'):
         raise KrakenAPIError(response['error'])
-    
+
     # Save raw Kraken API response in JSON
     save_raw_api_response_json(
         response=response,
-        directory=FILEPATHS['dir_raw_kraken_api_response_market_trades'],
-        filename=FILEPATHS['market_trades_raw'],
+        directory=PATHS['dirs']['raw_kraken_api_response_market_trades'],
+        filename=PATHS['files']['market_trades_raw'],
     )
 
     # Convert raw trade data into a PyArrow table
     raw_trades = response['result'][pair]
     table = generate_market_trades_table(raw_trades)
+
+    logger.info(f'Successfully generated market_trades table')
 
     return table
 
@@ -39,13 +48,15 @@ def ingest_order_book(pair: str = 'BTC/USD', count: int = 100):
     # Save raw Kraken API response in JSON
     save_raw_api_response_json(
         response=response,
-        directory=FILEPATHS['dir_raw_kraken_api_response_order_book'],
-        filename=FILEPATHS['order_book_raw'],
+        directory=PATHS['dirs']['raw_kraken_api_response_order_book'],
+        filename=PATHS['files']['order_book_raw'],
     )
 
     # Convert raw order book data into a PyArrow table
     raw_order_book = response['result'][pair]
     table = generate_order_book_table(raw_order_book)
+
+    logger.info(f'Successfully generated order_book table')
 
     return table
 
