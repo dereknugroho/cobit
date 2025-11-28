dd3a91a454f0e93f8514549819d3aace21f70e87
Derek <dereknugroho@gmail.com>
2025-11-28 13:57:14 -0400

    Refactor double quotes to single quotes, define global PATHS constant

--- src/cobit/utils/config.py
+++ src/cobit/utils/config.py
@@ -3,10 +3,10 @@ import json
 from importlib.resources import files
 
 def load_config():
-    config_path = files("cobit").joinpath("config.json")
+    config_path = files('cobit').joinpath('config.json')
     return json.loads(config_path.read_text())
 
 CONFIG = load_config()
 ENDPOINTS = CONFIG['endpoints']
-FILEPATHS = CONFIG['filepaths']
+PATHS = CONFIG['paths']
 NUM_FIELDS = CONFIG['num_fields']
