9f72104ce15fd56de3f553a1a70583fed5f5f25d
Derek <dereknugroho@gmail.com>
2025-11-28 14:39:55 -0400

    Remove trailing underscore from timestamp

--- src/cobit/utils/utils.py
+++ src/cobit/utils/utils.py
@@ -2,4 +2,4 @@ from datetime import datetime
 
 def generate_filename_timestamp() -> str:
     """Generate a timestamp 'yyyymmdd-hhmmss_' for prepending to filenames."""
-    return datetime.now().strftime('%Y%m%d-%H%M%S_')
+    return datetime.now().strftime('%Y%m%d-%H%M%S')
